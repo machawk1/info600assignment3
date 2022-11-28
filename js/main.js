54ab8d51fc0e1c1cbbadac41ba5eec0a286b64a4
Geoffrey Antony <geoffreyantony@Geoffreys-MacBook-Air.local>
2022-11-28 01:26:24 -0500

    new commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', assignClickHandler)
 
 function assignClickHandler () {
-  document.getElementById('addRec').addEventListener('click', function () {
-    const startYear = document.getElementById('startYear').value
-    if (startYear < 2000) {
+  const startYear = document.getElementById('startYear').value
+    if (startYear < 2000 || startYear > 2022) {
       window.alert('Incorrect year: ' + startYear)
       return
     }
+
     const fullName = document.getElementById('fullName').value
     const major = document.getElementById('major').value
 
@@ -15,25 +15,23 @@ function assignClickHandler () {
     const minutes = date.getMinutes().toString().padStart(2, '0')
     const time = hours + ':' + minutes
 
+    
     const newEntry = time + ' - ' + fullName + ', ' + major + ', ' + startYear
-
     const enteredRecords = document.getElementById('enteredRecords')
     let newChild = document.createElement('li')
     newChild.appendChild(document.createTextNode(newEntry))
 
     enteredRecords.appendChild(newChild)
 
-    document.getElementById('inputs').reset()
-  })
-
-  $.ajax({
-    type:"POST",
-    url: "user/",
-    data: {"fullName": fullName,
-           "major": major,
-           "startYear": startYear}
     
-})
+   $.ajax({
+       type:"POST",
+       url: "user/",
+       data: {"fullName": fullName,
+              "major": major,
+              "startYear": startYear}
+       
+  })
 }
 
 function addData(){
