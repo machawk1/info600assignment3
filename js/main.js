012d06ef1c62c8ec842ea70b3b14181e5b4222b2
Ayush Bajpai <119269052+fivetime999@users.noreply.github.com>
2022-11-28 17:14:27 -0500

    Js Changes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,94 @@
-document.addEventListener('DOMContentLoaded', assignClickHandler)
+function PostPerform(){
+   
+  const startYear = document.getElementById('startYear').value
+  if (startYear < 2000 || startYear > 2022) {
+    window.alert('Incorrect year: ' + startYear)
+    return
+  }
 
-function assignClickHandler () {
-  document.getElementById('addRec').addEventListener('click', function () {
-    const startYear = document.getElementById('startYear').value
-    if (startYear < 2000) {
-      window.alert('Incorrect year: ' + startYear)
-      return
-    }
-    const fullName = document.getElementById('fullName').value
-    const major = document.getElementById('major').value
+  const fullName = document.getElementById('fullName').value
+  const major = document.getElementById('major').value
 
-    const date = new Date()
-    const hours = date.getHours().toString().padStart(2, '0')
-    const minutes = date.getMinutes().toString().padStart(2, '0')
-    const time = hours + ':' + minutes
+  const date = new Date()
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const time = hours + ':' + minutes
 
-    const newEntry = time + ' - ' + fullName + ', ' + major + ', ' + startYear
+  
+  const newEntry = time + ' - ' + fullName + ', ' + major + ', ' + startYear
+  const enteredRecords = document.getElementById('enteredRecords')
+  let newChild = document.createElement('li')
+  newChild.appendChild(document.createTextNode(newEntry))
 
-    const enteredRecords = document.getElementById('enteredRecords')
-    let newChild = document.createElement('li')
-    newChild.appendChild(document.createTextNode(newEntry))
+  enteredRecords.appendChild(newChild)
 
-    enteredRecords.appendChild(newChild)
+  
+ $.ajax({
+     type:"POST",
+     url: "user/",
+     data: {"fullName": fullName,
+            "major": major,
+            "startYear": startYear}
+     
+})
 
-    document.getElementById('inputs').reset()
-  })
 }
+function Dataload(){
+
+    
+
+  $.ajax({
+      type: "GET",
+      url: "users",
+      dataType: "json",
+      success: function(data,status){
+         console.log(data)
+         
+         for(property in data)
+         {
+              console.log(property)
+              const fullName = document.getElementById('fullName').value
+              const major = document.getElementById('major').value
+              const date = new Date()
+              const hours = date.getHours().toString().padStart(2, '0')
+              const minutes = date.getMinutes().toString().padStart(2, '0')
+              const time = hours + ':' + minutes
+              const newEntry = time + ' - ' + fullName+ ', ' + major + ', ' + startYear
+              const enteredRecords = document.getElementById('enteredRecords')
+              let newChild = document.createElement('li')
+              newChild.appendChild(document.createTextNode(newEntry))
+
+              enteredRecords.appendChild(newChild)
+
+         }
+      }
+  });
+   
+ 
+}
+function Deletedata(){
+
+  $.ajax({
+    type: "DELETE",
+    url: "users",
+    dataType: "json",
+    success: function(data,status){
+       console.log(data)
+       
+       for(property in data)
+       {
+            const date = new Date()
+            const hours = date.getHours().toString().padStart(2, '0')
+            const minutes = date.getMinutes().toString().padStart(2, '0')
+            const time = hours + ':' + minutes
+            const newEntry = time + ' - ' + fullName.valueOf()+ ', ' + major.valueOf() + ', ' + startYear.valueOf()
+            const enteredRecords = document.getElementById('enteredRecords')
+            let newChild = document.createElement('li')
+            newChild.appendChild(document.createTextNode(newEntry))
+
+            enteredRecords.appendChild(newChild)
+       }
+
+    }
+  });
+   }
